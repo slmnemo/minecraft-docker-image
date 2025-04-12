3943c5c347495fcda4c2efa59ac6c715f29eb0cc
KaitlinLucio <kaitlin.verilog@gmail.com>
2025-04-12 02:52:25 -0700

    Updated man_server to use new definitions file

--- scheduled_restart.sh
+++ scheduled_restart.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "Entered '${SCRIPT_DIR}'"
+cd ${SCRIPT_DIR}
 ./man_server restart
